b72797ad8e7dd5d21c5c19f46a890b50ebf74bd7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CellContextMenu/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Grid;
+
+namespace CellContextMenu {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.ItemsSource = new ProductList();
+        }
+
+        private void copyCellDataItem_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e) {
+            GridCellMenuInfo menuInfo = view.GridMenu.MenuInfo as GridCellMenuInfo;
+            if (menuInfo != null && menuInfo.Row != null) {
+                string text = "" +
+                    grid.GetCellValue(menuInfo.Row.RowHandle.Value, (GridColumn)menuInfo.Column).ToString();
+                Clipboard.SetText(text);
+            }
+        }
+
+        private void deleteRowItem_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e) {
+            GridCellMenuInfo menuInfo = view.GridMenu.MenuInfo as GridCellMenuInfo;
+            if (menuInfo != null && menuInfo.Row != null)
+                view.DeleteRow(menuInfo.Row.RowHandle.Value);
+        }
+    }
+}
