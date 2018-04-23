f5fa8bd5e6e985d1e8754f5f77052a5b5958ae66
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to Display a Popup Menu for Data Cells
+
+
+<p>This example shows how to create a context menu displayed when an end user right-clicks a data cell. This menu allows an end-user to deleted the focused row or copy the focused cell's value to the clipboard.</p><br />
+
+
+<br/>
+
+
---
+++ VB/CellContextMenu/MainPage.xaml.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.Grid
+
+Namespace CellContextMenu
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.DataSource = New ProductList()
+		End Sub
+
+		Private Sub copyCellDataItem_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+			Dim menuInfo As GridCellMenuInfo = TryCast(view.GridMenu.MenuInfo, GridCellMenuInfo)
+			If menuInfo IsNot Nothing AndAlso menuInfo.Row IsNot Nothing Then
+				Dim text As String = "" & grid.GetCellValue(menuInfo.Row.RowHandle.Value, menuInfo.Column).ToString()
+				Clipboard.SetText(text)
+			End If
+		End Sub
+
+		Private Sub deleteRowItem_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+			Dim menuInfo As GridCellMenuInfo = TryCast(view.GridMenu.MenuInfo, GridCellMenuInfo)
+			If menuInfo IsNot Nothing AndAlso menuInfo.Row IsNot Nothing Then
+				view.DeleteRow(menuInfo.Row.RowHandle.Value)
+			End If
+		End Sub
+	End Class
+End Namespace
